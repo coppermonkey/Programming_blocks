040fe0458449e010f5d61f3fd539ee8e9fbf460f
Parth Shah <parths1229@gmail.com>
2021-10-01 23:25:06 +0530

    Add is_present methods

--- search_element.cpp
+++ search_element.cpp
@@ -10,6 +10,16 @@ void create_list(int rangestart, int rangeend, vector<int> &Alist)
 		cout<<i<<" "<<Alist[i];
 	}
 }
+
+void is_present (int key, vector<int> &Alist)
+{
+	for (int i=0; i<Alist.size(); i++) {
+		if (Alist.at(i) == key)
+			return 1;
+	}
+	return 0;
+}
+
 int main()
 {
 	vector<int> Alist;
@@ -17,4 +27,6 @@ int main()
 
 	for(vector<int>::iterator it = Alist.begin(); it!= Alist.end(); it++)
 		cout<<*it<<endl;
+
+	cout << is_present(-1, Alist);
 }

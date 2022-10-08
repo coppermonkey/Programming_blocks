a2a4ff2d530f81264a5af8ea5ccb3d3e7e857af0
Xuen Guo <x11storm@protonmail.com>
2022-10-09 00:23:05 +0530

    Sanitize and beautify the code

--- anagram.c
+++ anagram.c
@@ -4,39 +4,39 @@
  
 // function to count the frequency of each character
 //
-void count_frequency(char str[],int s[])
+void count_frequency(char inputString[],int s[])
 {
 	int i=0,j,count;
-	while(str[i]!='\0')
+	while(inputString[i]!='\0')
 	{
-		j=0;
-		count=0;
-		while(str[j]!='\0')
+		j = 0;
+		count = 0;
+		while(inputString[j] != '\0')
 		{
-			if(str[i]==str[j])
+			if(inputString[i] == inputString[j])
 				count++;
 			j++;
 		}
-		s[str[i]-97]=count;
+		s[inputString[i]-97] = count;
 		i++;
 	}
 }
 
 int main()
 {
-	char str1[100],str2[100];
+	char inputString1[100],inputString2[100];
 	int i,j,flag=1,s1[26]={0},s2[26]={0};
-	printf("Enter first string:");
-	scanf("%s",str1);
-	printf("Enter second string:");
-	scanf("%s",str2);
-	if(strlen(str1)!=strlen(str2)) //if the lengths of two strings are not equal
+	printf("Enter first inputStringing:");
+	scanf("%s",inputString1);
+	printf("Enter second inputStringing:");
+	scanf("%s",inputString2);
+	if(inputStringlen(inputString1)!=inputStringlen(inputString2)) //if the lengths of two inputStringings are not equal
 	{
 		printf("\nStrings are not anagrams");
 		exit(0);
 	}
-	count_frequency(str1,s1);
-	count_frequency(str2,s2);
+	count_frequency(inputString1,s1);
+	count_frequency(inputString2,s2);
 	for(i=0;i<26;++i) //checking freuency of each character
 	{
 		if(s1[i]!=s2[i])

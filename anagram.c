944acb2148da8f7095017c75cdb2eef8e07e154c
Xuen Guo <x11storm@protonmail.com>
2022-10-09 00:26:05 +0530

    [anagram.c] Sanitize and beautify the code

--- anagram.c
+++ anagram.c
@@ -25,27 +25,35 @@ void count_frequency(char inputString[],int s[])
 int main()
 {
 	char inputString1[100],inputString2[100];
-	int i,j,flag=1,s1[26]={0},s2[26]={0};
+	int i,j,flag = 1;
+	int tempString[26] = {0}, auxString[26] = {0};
+
 	printf("Enter first inputStringing:");
 	scanf("%s",inputString1);
 	printf("Enter second inputStringing:");
 	scanf("%s",inputString2);
-	if(inputStringlen(inputString1)!=inputStringlen(inputString2)) //if the lengths of two inputStringings are not equal
+	
+	// if the length of the two strings are not equal
+	if (inputStringlen(inputString1) != inputStringlen(inputString2))
 	{
 		printf("\nStrings are not anagrams");
 		exit(0);
 	}
-	count_frequency(inputString1,s1);
-	count_frequency(inputString2,s2);
-	for(i=0;i<26;++i) //checking freuency of each character
+
+	count_frequency(inputString1,tempString);
+	count_frequency(inputString2,auxString);
+
+	//checking frequency of each character
+	for (i=0 ; i<26 ; ++i)
 	{
-		if(s1[i]!=s2[i])
+		if(tempString[i] != auxString[i])
 		{
-			flag=0;
+			flag = 0;
 			break;
 		}
 	}
-	if(flag)
+
+	if (flag)
 		printf("\nStrings are anagrams");
 	else
 		printf("\nStrings are not anagrams");
